4f04b963fbdd21e0814086c7e9f97fd204c2ce22
quiqid <quiqid@outlook.com>
2024-09-27 00:15:11 +0700

    LR1 - Job's done

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val inputChar = findViewById<EditText>(R.id.inputChar)
+        val checkButton = findViewById<Button>(R.id.checkButton)
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+
+        checkButton.setOnClickListener {
+            val input = inputChar.text.toString()
+
+            if (input.length != 1) {
+                Toast.makeText(this, "Пожалуйста, введите один символ", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val char = input[0]
+
+            // Проверка, является ли символ латинской прописной буквой
+            if (char in 'A'..'Z') {
+                when (char) {
+                    'L', 'M', 'K', 'D' -> resultTextView.text = "Это согласные буквы"
+                    else -> resultTextView.text = "Возможно, это гласные буквы"
+                }
+            } else {
+                Toast.makeText(this, "Символ не является латинской прописной буквой", Toast.LENGTH_SHORT).show()
+                resultTextView.text = ""
+            }
         }
     }
 }
