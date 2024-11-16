62348f57f4ee57659bef34b66c63671f4e468ff1
quiqid <quiqid@outlook.com>
2024-11-16 11:02:58 +0700

    LR4_2 - Added layout of CheatActivity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonTrue: Button
+    private lateinit var buttonFalse: Button
+    private lateinit var questionTextView: TextView
+    private lateinit var answerTextView: TextView
+    private lateinit var buttonNext: Button
+    private lateinit var buttonCheat: Button
+
+    private var currentQuestionIndex = 0
+    private var correctAnswersCount = 0
+    private var cheatCount = 0
+    private var isAnswered = false
+    private var answerText = ""
+
+    private val questions = listOf(
+        Pair("Все медузы являются ядовитыми?", false),
+        Pair("Птицы являются потомками динозавров?", true),
+        Pair("Вода кипит при температуре 100 градусов Цельсия на любой высоте?", false),
+        Pair("Слон — это самое крупное сухопутное животное на планете?", true)
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
