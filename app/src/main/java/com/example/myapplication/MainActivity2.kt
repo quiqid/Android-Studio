bf9b3250b32f71e5e3df48a38775a562642c2fbb
quiqid <quiqid@outlook.com>
2024-11-16 11:05:00 +0700

    LR4_2 - Added functionality for CheatActivity

--- app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -1,20 +1,40 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.os.Build
+
 
 class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var androidInfo: TextView
+    private lateinit var textViewAnswer: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        androidInfo = findViewById(R.id.textViewApi)
+        textViewAnswer = findViewById(R.id.textViewCheat)
+
+        val questionIndex = intent.getIntExtra("questionIndex", 0)
+        val correctAnswer = intent.getBooleanExtra("correctAnswer", false)
+
+        textViewAnswer.text = "Правильный ответ: $correctAnswer"
+
+        val androidVersion = getAndroidVersion()
+        androidInfo.text = androidVersion
+
+    }
+
+    private fun getAndroidVersion(): String {
+        return "Android Version: ${Build.VERSION.RELEASE}, API Level: ${Build.VERSION.SDK_INT}"
     }
 }
