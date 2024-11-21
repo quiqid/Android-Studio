f49a22d70d59cb9c1f83d41117eb1adec4b9b1ce
quiqid <quiqid@outlook.com>
2024-11-21 19:51:28 +0700

    LR5 - Added functionality for MainActivity2

--- app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -1,12 +1,18 @@
 package com.example.myapplication
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
 
+class PaymentActivity : AppCompatActivity() {
 
-class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val paymentTextView: TextView = findViewById(R.id.paymentTextView)
+        val totalPrice = intent.getIntExtra("TOTAL_PRICE", 0)
+
+        paymentTextView.text = "Оплатить $totalPrice рублей."
     }
 }
